f4f5e8b9b6dfcff38502ff8045a589f3eda7d151
recor-glitch <rishisarmah4@gmail.com>
2025-10-31 21:03:09 +0530

    empty career listing page done

--- src/app/[locale]/career/error.tsx
+++ src/app/[locale]/career/error.tsx
@@ -4,17 +4,19 @@ import Image from "next/image";
 import React from "react";
 import NoCareer from "../../../../public/no-career.svg";
 import CTA from "@/components/Landing/CTA";
+import { useTranslations } from "next-intl";
 
 function CareerErrorPage() {
+  const t = useTranslations("career.error");
   return (
     <div className="min-h-screen bg-background text-center grid grid-cols-3 container mx-auto px-4 py-16">
       <h1 className="text-6xl col-span-full text-primary flex justify-center items-center">
-        Nothing now doesn’t mean never.
+        {t("title")}
       </h1>
       <div className="col-span-full m-auto">
         <Image
           src={NoCareer}
-          alt={"No Career image"}
+          alt={t("alt")}
           height={400}
           width={400}
           className="max-h-[846px]"
